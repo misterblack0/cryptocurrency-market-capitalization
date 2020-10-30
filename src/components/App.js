416d886c4fa13323471e656c6a2cf11a4b3a8559
misterblack0 <misterblack0@protonmail.com>
2020-10-30 21:01:30 +0200

    added defi data

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./Nav";
 import Home from "./Home";
 import Exchanges from "./Exchanges";
+import DefiData from "./DefiData";
 
 const App = () => (
   <Router>
+  <DefiData/>
     <Nav />
     <Switch>
       <Route path="/" exact component={Home} />
